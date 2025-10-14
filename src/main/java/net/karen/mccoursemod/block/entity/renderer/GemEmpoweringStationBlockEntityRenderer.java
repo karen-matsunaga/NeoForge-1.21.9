3fbe2162c3db4c89497e3ca83790043e2fb6031c
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-10-14 13:28:16 -0300

    2025-10-14 [Demo] Fixed Gem Empowering Station block renderer.

--- src/main/java/net/karen/mccoursemod/block/entity/renderer/GemEmpoweringStationBlockEntityRenderer.java
+++ src/main/java/net/karen/mccoursemod/block/entity/renderer/GemEmpoweringStationBlockEntityRenderer.java
@@ -4,7 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import net.karen.mccoursemod.block.custom.GemEmpoweringStationBlock;
 import net.karen.mccoursemod.block.entity.GemEmpoweringStationBlockEntity;
-import net.karen.mccoursemod.block.entity.render.GemEmpoweringStationBlockEntityRenderState;
+import net.karen.mccoursemod.block.entity.renderstate.GemEmpoweringStationBlockEntityRenderState;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
@@ -16,15 +16,14 @@ import net.minecraft.client.renderer.state.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class GemEmpoweringStationBlockEntityRenderer implements BlockEntityRenderer<GemEmpoweringStationBlockEntity,
-                                                                GemEmpoweringStationBlockEntityRenderState> {
+public class GemEmpoweringStationBlockEntityRenderer
+       implements BlockEntityRenderer<GemEmpoweringStationBlockEntity, GemEmpoweringStationBlockEntityRenderState> {
     private final ItemModelResolver itemModelResolver;
 
     public GemEmpoweringStationBlockEntityRenderer(BlockEntityRendererProvider.Context context) {
@@ -32,20 +31,27 @@ public class GemEmpoweringStationBlockEntityRenderer implements BlockEntityRende
     }
 
     @Override
-    public void extractRenderState(@NotNull GemEmpoweringStationBlockEntity blockEntity,
+    public void extractRenderState(@NotNull GemEmpoweringStationBlockEntity entity,
                                    @NotNull GemEmpoweringStationBlockEntityRenderState state,
                                    float partialTick, @NotNull Vec3 cameraPosition,
                                    @Nullable ModelFeatureRenderer.CrumblingOverlay breakProgress) {
-        state.gemEmpoweringStationBlockEntity = blockEntity;
-        state.itemStack = blockEntity.getRenderStack();
-        state.level = blockEntity.getLevel();
-        BlockEntityRenderer.super.extractRenderState(blockEntity, state, partialTick, cameraPosition, breakProgress);
+        BlockEntityRenderer.super.extractRenderState(entity, state, partialTick, cameraPosition, breakProgress);
+        state.gemEmpoweringStationBlockEntity = entity;
+        state.itemStack = entity.getRenderStack();
+        state.gemEmpoweringStationBEBlockPos = state.gemEmpoweringStationBlockEntity.getBlockPos();
+        state.gemEmpoweringStationBEBlockState = state.gemEmpoweringStationBlockEntity.getBlockState();
+        state.level = entity.getLevel();
+        if (state.level != null) { // Render item
+            state.lightCoords = getLightLevel(state.level, state.gemEmpoweringStationBEBlockPos);
+            this.itemModelResolver.updateForTopItem(state.item, state.itemStack, ItemDisplayContext.FIXED,
+                                                    state.level, null, 1);
+        }
     }
 
     private int getLightLevel(Level level, BlockPos pos) {
-        int bLight = level.getBrightness(LightLayer.BLOCK, pos);
-        int sLight = level.getBrightness(LightLayer.SKY, pos);
-        return LightTexture.pack(bLight, sLight);
+        int blockLight = level.getBrightness(LightLayer.BLOCK, pos);
+        int skyLight = level.getBrightness(LightLayer.SKY, pos);
+        return LightTexture.pack(blockLight, Math.max(skyLight, 15));
     }
 
     @Override
@@ -54,24 +60,16 @@ public class GemEmpoweringStationBlockEntityRenderer implements BlockEntityRende
     }
 
     @Override
-    public void submit(GemEmpoweringStationBlockEntityRenderState blockEntity, PoseStack poseStack,
+    public void submit(GemEmpoweringStationBlockEntityRenderState state, PoseStack poseStack,
                        @NotNull SubmitNodeCollector submitNodeCollector, @NotNull CameraRenderState cameraRenderState) {
-        ItemStackRenderState item = blockEntity.item;
-        ItemStack itemStack = blockEntity.itemStack;
-        GemEmpoweringStationBlockEntity gemEmpoweringStationBlockEntity = blockEntity.gemEmpoweringStationBlockEntity;
-        Level level = blockEntity.level;
+        ItemStackRenderState item = state.item;
         poseStack.pushPose();
         poseStack.translate(0.5F, 0.75F, 0.5F);
         poseStack.scale(0.35F, 0.35F, 0.35F);
-        poseStack.mulPose(Axis.YN.rotationDegrees(gemEmpoweringStationBlockEntity.getBlockState()
-                                                                                 .getValue(GemEmpoweringStationBlock.FACING)
-                                                                                 .toYRot()));
+        poseStack.mulPose(Axis.YN.rotationDegrees(state.gemEmpoweringStationBEBlockState
+                                                       .getValue(GemEmpoweringStationBlock.FACING).toYRot()));
         poseStack.mulPose(Axis.XP.rotationDegrees(270));
-        if (level != null) { // Render item
-            blockEntity.lightCoords = getLightLevel(level, gemEmpoweringStationBlockEntity.getBlockPos());
-            this.itemModelResolver.updateForTopItem(item, itemStack, ItemDisplayContext.FIXED, level, null, 1);
-        }
-        item.submit(poseStack, submitNodeCollector, blockEntity.lightCoords, OverlayTexture.NO_OVERLAY, 1);
+        item.submit(poseStack, submitNodeCollector, state.lightCoords, OverlayTexture.NO_OVERLAY, 0);
         poseStack.popPose();
     }
 }
