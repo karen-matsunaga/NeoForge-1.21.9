54d215db67a3cb6b8ade3a4fc339cb4889b2776e
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-11-07 17:35:59 -0300

    2025-11-07 Improved Mod Villager codes and comments

--- src/main/java/net/karen/top/villager/ModVillagers.java
+++ src/main/java/net/karen/top/villager/ModVillagers.java
@@ -18,33 +18,32 @@ public class ModVillagers {
     public static final DeferredRegister<PoiType> POI_TYPES =
            DeferredRegister.create(BuiltInRegistries.POINT_OF_INTEREST_TYPE, Top.MOD_ID);
 
-    // Register all custom villager professions types
+    // Register all custom villager PROFESSIONS types
     public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS =
            DeferredRegister.create(BuiltInRegistries.VILLAGER_PROFESSION, Top.MOD_ID);
 
     // Register all custom villager POI
     // KAUPEN POI
-    public static final Holder<PoiType> KAUPEN_POI = POI_TYPES.register("kaupen_poi",
-            () -> new PoiType(ImmutableSet.copyOf(ModBlocks.CHAIR.get().getStateDefinition()
-                                                                       .getPossibleStates()), 1, 1));
+    public static final Holder<PoiType> KAUPEN_POI = POI_TYPES.register("kaupen_poi", () ->
+           new PoiType(ImmutableSet.copyOf(ModBlocks.CHAIR.get().getStateDefinition().getPossibleStates()),
+                       1, 1));
 
     // SOUND POI
-    public static final Holder<PoiType> SOUND_POI = POI_TYPES.register("sound_poi",
-           () -> new PoiType(ImmutableSet.copyOf(ModBlocks.SOUND.get().getStateDefinition()
-                                                                      .getPossibleStates()), 1, 1));
+    public static final Holder<PoiType> SOUND_POI = POI_TYPES.register("sound_poi", () ->
+           new PoiType(ImmutableSet.copyOf(ModBlocks.SOUND.get().getStateDefinition().getPossibleStates()),
+                       1, 1));
 
     // Register all custom villager professions
     // KAUPENGER
     public static final Holder<VillagerProfession> KAUPENGER = VILLAGER_PROFESSIONS.register("kaupenger",
-            () -> new VillagerProfession(Component.literal("kaupenger"),
-                                         holder -> holder.value() == KAUPEN_POI.value(),
-                                         poiTypeHolder -> poiTypeHolder.value() == KAUPEN_POI.value(),
-                                         ImmutableSet.of(), ImmutableSet.of(), ModSounds.MAGIC_BLOCK_HIT.get()));
+           () -> new VillagerProfession(Component.translatable("entity.minecraft.villager.top.kaupenger"),
+                                        holder -> holder.value() == KAUPEN_POI.value(),
+                                        poiTypeHolder -> poiTypeHolder.value() == KAUPEN_POI.value(),
+                                        ImmutableSet.of(), ImmutableSet.of(), ModSounds.MAGIC_BLOCK_HIT.get()));
 
     // SOUND MASTER
-    public static final Holder<VillagerProfession> SOUND_MASTER =
-           VILLAGER_PROFESSIONS.register("soundmaster",
-           () -> new VillagerProfession(Component.literal("soundmaster"),
+    public static final Holder<VillagerProfession> SOUND_MASTER = VILLAGER_PROFESSIONS.register("soundmaster",
+           () -> new VillagerProfession(Component.translatable("entity.minecraft.villager.top.soundmaster"),
                                         holder -> holder.value() == SOUND_POI.value(),
                                         poiTypeHolder -> poiTypeHolder.value() == SOUND_POI.value(),
                                         ImmutableSet.of(), ImmutableSet.of(), SoundEvents.VILLAGER_WORK_WEAPONSMITH));
